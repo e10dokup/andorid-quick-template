1f84f2b43f8b6e07d43d2c87ab7745505b39b87a
Yoshihiro WADA <1365196t@stu.kobe-u.ac.jp>
2016-04-08 12:10:02 +0900

    Remove unused methods

--- app/src/main/java/info/e10dokup/androidquicktemplate/core/BaseFragment.java
+++ app/src/main/java/info/e10dokup/androidquicktemplate/core/BaseFragment.java
@@ -9,17 +9,6 @@ public abstract class BaseFragment extends Fragment {
     private static final String TAG = BaseFragment.class.getSimpleName();
     private final BaseFragment self = this;
 
-    boolean mFirstTime = true;
-    boolean mAutoTracking = true;
-
-    public void setAutoTracking(boolean autoTracking) {
-        mAutoTracking = autoTracking;
-    }
-
-    public String getTrackingParams() {
-        return "";
-    }
-
     public void replaceFragment(BaseFragment fragment, boolean recordBackstack) {
         getBaseActivity().replaceFragment(fragment, recordBackstack);
     }
