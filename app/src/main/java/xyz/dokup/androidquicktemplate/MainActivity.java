a216134965e66b87bc2e06b501f8be9f2daf64b0
Yoshihiro WADA <1365196t@stu.kobe-u.ac.jp>
2016-05-30 12:53:13 +0900

    Fix theme

--- app/src/main/java/xyz/dokup/androidquicktemplate/MainActivity.java
+++ app/src/main/java/xyz/dokup/androidquicktemplate/MainActivity.java
@@ -3,16 +3,26 @@ package xyz.dokup.androidquicktemplate;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import xyz.dokup.androidquicktemplate.core.BaseActivity;
 import xyz.dokup.androidquicktemplate.core.BaseFragment;
 
 public class MainActivity extends BaseActivity {
 
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mToolbar.setTitle(R.string.app_name);
     }
 
     @Override
